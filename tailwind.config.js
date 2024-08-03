19c5dcb111b2dd22b8269f1a264ff310d6fe5617
Subham Tiwari <118207456+subham2913@users.noreply.github.com>
2024-08-03 11:22:59 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        white: "white",
+        none: "none",
+      },
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      borderWidth: {
+        1: "1px",
+      },
+      gridTemplateRows: {
+        7: "repeat(7, minmax(0, 1fr))",
+        8: "repeat(8, minmax(0, 1fr))",
+      },
+    },
+  },
+  plugins: [],
+}
+
